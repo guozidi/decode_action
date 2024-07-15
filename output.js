b2e2d2e708ff950021e24406be7313ea6ad37b8c
action <action@github.com>
2024-07-15 04:32:56 +0000

    Add decoded output file

--- output.js
+++ output.js
@@ -1,4 +1,4 @@
-//Mon Jul 01 2024 07:11:33 GMT+0000 (Coordinated Universal Time)
+//Mon Jul 15 2024 04:32:56 GMT+0000 (Coordinated Universal Time)
 //Base:https://github.com/echo094/decode-js
 //Modify:https://github.com/smallfawn/decode_action
 const _0x40bf46 = _0x35a552("携程旅行"),
